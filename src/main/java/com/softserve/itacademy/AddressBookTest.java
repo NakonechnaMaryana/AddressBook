00878ff2cc46acafa6fe2136a6dca4161de656a9
NakonechnaMaryana <mariananakonechna@gmail.com>
2020-08-07 14:34:31 +0300

    Initial commit

---
+++ src/main/java/com/softserve/itacademy/AddressBookTest.java
@@ -0,0 +1,30 @@
+package com.softserve.itacademy;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AddressBookTest {
+
+    @org.junit.jupiter.api.Test
+    void create() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void read() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void update() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void delete() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void sortedBy() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getInstance() {
+    }
+}
